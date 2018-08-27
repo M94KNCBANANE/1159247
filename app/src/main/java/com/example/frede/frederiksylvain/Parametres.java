250fd62c3c980470715fd8e999b3589554e8f0c6
Frederik sylvain <frederik_coolss@hotmail.com>
2018-08-27 15:38:39 -0400

    Affiche un message dépendament de l'orientation et de la langue

--- app/src/main/java/com/example/frede/frederiksylvain/Parametres.java
+++ app/src/main/java/com/example/frede/frederiksylvain/Parametres.java
@@ -1,5 +1,6 @@
 package com.example.frede.frederiksylvain;
 
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,12 @@ public class Parametres extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parametres);
-        Log.d("MonEtiquette","Bonjour!");
+        Log.d("MonEtiquette",this.getResources().getString(R.string.Bonjour));
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+            Log.d("MonEtiquette",this.getResources().getString(R.string.Bonjour) + " Paysage");
+        }
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT){
+            Log.d("MonEtiquette",this.getResources().getString(R.string.Bonjour) + " Portait");
+        }
     }
 }
